6503d586db136592ea58e030214d43b850a0290e
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2015-05-28 00:34:15 -0700

    Updated to full page caching

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# module-memcache
+# memcache module
+
+* Date:    May 28, 2016
+* Author:  Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
+* Version: 2.0
+* License: http://opensource.org/licenses/MIT
+
+Memcache module uses a memcache server to cache full pages in memory. This helps
+the page load faster, especially when the page is generated using databases or
+when using computationally intensive algorithms.
+
+## Changelog
+
+### Version 2.0
+
+Version jumped to 2.0 because there was a fundamental change in how memcache 
+works. Previously, it cached on a template bases, but it was buggy in certain
+edge cases so we reverted to a full page cache.
+
+## Usage
+
+No user configuration necessary.
--- memcache.php
+++ memcache.php
@@ -3,23 +3,24 @@ namespace Module\Memcache;
 
 /**
  * Caches the page
- * @param  string $page A unique identifier for a page
- * @return string       A html page
+ * @return void
  * @internal
  */
-function manage_cached_page($page) {
+function cache_page() {
 	$port = 11211;
-	$cacheExpires = 10; //seconds
+	$cacheExpires = 30; //seconds
 
+	$url = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+	$page = ob_get_contents();
 	$memcache = new \Memcache;
 	$memcache->connect('localhost', $port) or die ("Memcache: Could not connect");
 
-	if (!$get_result = $memcache->get(__FILE__)) {
-		$get_result = $memcache->set(__FILE__, $page, 0, $cacheExpires) or
+	if (!$get_result = $memcache->get($url)) {
+		$get_result = $memcache->set($url, $page, 0, $cacheExpires) or
 		die("Failed to save data at the server");
 	}
 
-	return $page;
+	ob_end_flush();
 }
 
 /**
@@ -29,18 +30,22 @@ function manage_cached_page($page) {
  */
 function show_cached_page() {
 	$port = 11211;
+	$url = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 	$memcache = new \Memcache;
 	$memcache->connect('localhost', $port) or die ("Memcache: Could not connect");
 
-	if ($get_result = $memcache->get(__FILE__)) {
+	if ($get_result = $memcache->get($url)) {
 		header('Memcached: true');
 		echo $get_result;
 		die();
 	}
+
+	ob_start();
 }
 
-if (ENV === "LIVE") {
-	\Sleepy\Hook::applyFilter('render_template', '\Module\Memcache\manage_cached_page');
-	\Sleepy\Hook::doAction('sleepy_preprocess', '\Module\Memcache\show_cached_page');
+if (ENV !== "LIVE") {
+	\Sleepy\Hook::doAction('sleepy_preprocess',  '\Module\Memcache\show_cached_page');
+	\Sleepy\Hook::doAction('sleepy_postprocess', '\Module\Memcache\cache_page');
 }
+
